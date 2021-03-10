0103ebe098f1224a8e83de03f782d2d0b699ddcd
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2021-03-10 14:02:09 -0300

    Working on a fix to the twitter version

--- backend/src/routes/categories.ts
+++ backend/src/routes/categories.ts
@@ -1,23 +1,34 @@
 import { Router } from 'express';
 
-import { searchGoogle, searchYoutube } from '../utils/scraper';
+import { searchGoogle, searchYoutube, searchTwitterPeople, searchTwitterTopics } from '../utils/scraper';
 
 const router = Router();
 
 router.get('/:categoryName', async (req, res) => {
     const { categoryName }: { categoryName?: string } = req.params;
 
-    const googleSearch = await searchGoogle(categoryName);
-    const youtubeSearch = await searchYoutube(categoryName);
-    const podcastSearch = await searchGoogle(categoryName + ' podcast');
+    // const googleSearch = await searchGoogle(categoryName);
+    // const youtubeSearch = await searchYoutube(categoryName);
+    // const podcastSearch = await searchGoogle(categoryName + ' podcast');
+    // const twitterSearchPeople = await searchTwitterPeople(categoryName);
+    const twitterSearchTopics = await searchTwitterTopics(categoryName);
 
-    const result = {
-        titles: [...googleSearch.titles, ...youtubeSearch.titles, ...podcastSearch.titles],
-        links: [...googleSearch.links, ...youtubeSearch.links, ...podcastSearch.links],
-        descriptions: [...googleSearch.descriptions, ...youtubeSearch.descriptions, ...podcastSearch.descriptions]
-    }
+    console.log(twitterSearchTopics);
+    
+    return res.json(twitterSearchTopics);
 
-    return res.json(result);
+
+    /* TODO: Reddit, twitter and amazon. */
+
+    // const result = {
+    //     titles: [...googleSearch.titles, ...youtubeSearch.titles, ...podcastSearch.titles],
+    //     links: [...googleSearch.links, ...youtubeSearch.links, ...podcastSearch.links],
+    //     descriptions: [...googleSearch.descriptions, ...youtubeSearch.descriptions, ...podcastSearch.descriptions]
+    // }
+
+    // console.log(result);
+
+    // return res.json(result);
 });
 
 export default router;
--- backend/src/utils/scraper.ts
+++ backend/src/utils/scraper.ts
@@ -63,8 +63,6 @@ const searchYoutube = async (searchQuery: string) => {
     await page.goto('https://www.youtube.com/results?search_query=' + searchQuery, { waitUntil: 'networkidle2' });
     await page.waitForSelector('div[id=container]');
 
-    await page.screenshot({ path: 'screenshot.png' });
-
     /* Evaluate all titles, mapping one by one. Getting the link afterwards. */
     (await page.$$('a > yt-formatted-string')).map(async result => {
 
@@ -81,8 +79,6 @@ const searchYoutube = async (searchQuery: string) => {
             data.titles.push(title);
             data.descriptions.push('No description available.')
         }
-
-        console.log(data);
     });
 
     await browser.close();
@@ -90,4 +86,86 @@ const searchYoutube = async (searchQuery: string) => {
     return data;
 }
 
-export { searchGoogle, searchYoutube };
+const searchTwitterPeople = async (searchQuery: string) => {
+    const data: IScraperData = { titles: [], links: [], descriptions: [] }
+
+    const browser = await puppeteer.launch({
+        args: [
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-first-run',
+            '--no-sandbox',
+            '--no-zygote',
+            '--single-process',
+        ]
+    });
+    const page = await browser.newPage();
+    await page.goto('https://twitter.com/search?q=' + searchQuery + '&src=typed_query&f=user', { waitUntil: 'networkidle2' });
+    await page.waitForSelector('main[role=main]');
+
+    /* Evaluate all titles, mapping one by one. Getting the link afterwards. */
+    (await page.$$('a > div > div > div > span > span')).map(async result => {
+
+        const evaluation = await result.evaluate(element => element.textContent + '\n' + element.parentElement.parentElement.parentElement.parentElement.parentElement.href);
+
+        const [ title, link ] = evaluation.split('\n');
+
+        /* Avoid empty links / titles. */
+        if (!title || !link) return;
+
+        data.links.push(link);
+        data.titles.push(title);
+        data.descriptions.push('Twitter profile.');
+    });
+
+    await browser.close();
+
+    return data;
+}
+
+const searchTwitterTopics = async (searchQuery: string) => {
+    const data: IScraperData = { titles: [], links: [], descriptions: [] }
+
+    const browser = await puppeteer.launch({
+        args: [
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-first-run',
+            '--no-sandbox',
+            '--no-zygote',
+            '--single-process',
+        ]
+    });
+    const page = await browser.newPage();
+    await page.goto('https://twitter.com/search?q=' + searchQuery + '&src=typed_query', { waitUntil: 'networkidle2' });
+    await page.waitForSelector('main[role=main]');
+
+    /* Evaluate all titles, mapping one by one. Getting the link afterwards. */
+    (await page.$$('div[data-testid="tweet"] > div > div div[lang=en] > span')).map(async (result, index) => {
+
+        /**
+            * We want to iterate over even amounts of tweets, since the next span ( the odd one )
+            would be a bold text with the search query
+        **/
+        if (index % 2 !== 0) return;
+
+        /* If it starts with '#', it's not a text worth showing. */
+        if (result.evaluate(element => element.textContent.startsWith('#') ? false : element.textContent)) {
+            const title = await result.evaluate(element => element.textContent);
+
+            if (!title) return;
+
+            data.titles.push(title);
+            data.links.push('https://twitter.com/search?q=' + searchQuery + '&src=typed_query');
+            data.descriptions.push('Twitter topic.');
+        }
+    });
+
+    await browser.close();
+
+    return data;
+}
+
+export { searchGoogle, searchYoutube, searchTwitterPeople, searchTwitterTopics };
