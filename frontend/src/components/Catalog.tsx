9d42db320f6816812e1a26aaba21b9954af3729d
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2021-03-12 11:01:03 -0300

    Improved frontend

--- frontend/src/components/Catalog.tsx
+++ frontend/src/components/Catalog.tsx
@@ -6,20 +6,36 @@ const Index: React.FC = () => {
     return (
         <div className='container-parent'>
             <div className='container-categories'>
-                <div className='category'>
-                    <a className="category-link" href="/categories/programming">Programming</a>
+                <div className='category' style={{ backgroundColor: '#0d89a3' }}>
+                    <a className="category-link" href="/categories/programming">
+                        <i className="fab fa-python icon"></i>
+                        <br/>
+                        Programming
+                    </a>
                 </div>
 
-                <div className="category">
-                    <a className='category-link' href="/categories/medicine">Medicine</a>
+                <div className="category" style={{ backgroundColor: '#bc053e' }}>
+                    <a className='category-link' href="/categories/medicine">
+                        <i className="fas fa-clinic-medical icon"></i>
+                        <br/>
+                        Medicine
+                    </a>
                 </div>
 
-                <div className="category">
-                    <a className='category-link' href="/categories/history">History</a>
+                <div className="category" style={{ backgroundColor: '#661e6a' }}>
+                    <a className='category-link' href="/categories/history">
+                        <i className="fas fa-book icon"></i>
+                        <br/>
+                        History
+                    </a>
                 </div>
 
-                <div className="category">
-                    <a className='category-link' href="/categories/finances">Finances</a>    
+                <div className="category" style={{ backgroundColor: '#377e72' }}>
+                    <a className='category-link' href="/categories/finances">
+                        <i className="far fa-money-bill-alt icon"></i>
+                        <br/>
+                        Finances
+                    </a>    
                 </div>
 
             </div>
